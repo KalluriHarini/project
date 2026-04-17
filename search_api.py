3d0874ac7a24441b68367c3280a46552914f0cce
KalluriHarini <kalluriharini818@gmail.com>
2026-04-17 20:18:48 +0530

    Update search_api.py

--- search_api.py
+++ search_api.py
@@ -175,3 +175,7 @@ def search():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
